b705d84314b41ccd9e854de3639fc5266b5cca3f
Константин <basov04@mail.ru>
2023-11-15 11:21:29 +0300

    Api-gateway finish

--- eureka-client/src/main/java/ru/javabegin/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/javabegin/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package ru.javabegin.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class TestController {
+    @Value("${eureka.instance.instance-id}")
+    private String id;
     @GetMapping("/test")
     public String test(){
-        return "Hello!";
+        return id;
     }
 }
